18628b950134a814f79b5a323ee2b62fc78cef79
xafero <xafero@users.noreply.github.com>
2026-01-15 00:09:45 +0100

    Adding gen

---
+++ src/Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
