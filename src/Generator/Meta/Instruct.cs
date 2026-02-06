33da906356c027b88583ea4c4db5fcafc24d0bd9
xafero <xafero@users.noreply.github.com>
2026-02-06 22:37:15 +0100

    Deleting old comments

--- src/Generator/Meta/Instruct.cs
+++ src/Generator/Meta/Instruct.cs
@@ -7,14 +7,22 @@ namespace Generator.Meta
 {
     public sealed record Instruct
     {
-        [Index(0)] public string? Format { get; set; }
+        [Index(0)] public string? Group { get; set; }
 
-        [Index(1)] public string? Summary { get; set; }
+        [Index(1)] public string? Label { get; set; }
 
-        [Index(2)] public string? Instruction { get; set; }
+        [Index(2)] public string? Description { get; set; }
 
-        [Index(3)] public string? States { get; set; }
+        [Index(3)] public string? Format { get; set; }
 
-        [Index(4)] public string? TBit { get; set; }
+        [Index(4)] public string? Summary { get; set; }
+
+        [Index(5)] public string? Instruction { get; set; }
+
+        [Index(6)] public string? States { get; set; }
+
+        [Index(7)] public string? TBit { get; set; }
+        
+        [Index(8)] public string? UsedIn { get; set; }
     }
 }
